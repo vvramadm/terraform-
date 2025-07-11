7b2e567d547e4ddebff0a6cf117679b96de4fd4e
vvramadm <vvramadm@gmail.com>
2025-07-11 17:47:12 +0530

    terraform

--- expense-infra-dev/60-backend/backend.sh
+++ expense-infra-dev/60-backend/backend.sh
@@ -2,5 +2,5 @@
 component=$1
 environment=$2
 echo "component : $component, Environment : $environment "
-dns install ansible -y
+dnf install ansible -y
 ansible-pull -i localhost, -U https://github.com/vvramadm/expense-ansible-tf.git main.yaml -e component=$component -e environment=$environment
