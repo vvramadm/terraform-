fcab825b49bda7b88bd6f8e71e351a57bb4c12ce
vvramadm <vvramadm@gmail.com>
2025-07-13 15:39:09 +0530

    terraform

--- expense-infra-dev/80-web-alb/data.tf
+++ expense-infra-dev/80-web-alb/data.tf
@@ -12,6 +12,6 @@ data "aws_ssm_parameter" "web_alb_sg_id" {
   name = "/${var.project_name}/${var.environment}/web_alb_sg_id"
 }
 
-data "aws_ssm_parameter" "https_certificate_arn" {
-name  = "/${var.project_name}/${var.environment}/https_certificate_arn"
-}
+# data "aws_ssm_parameter" "https_certificate_arn" {
+# name  = "/${var.project_name}/${var.environment}/https_certificate_arn"
+# }
